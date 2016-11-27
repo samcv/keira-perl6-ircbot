0251076c3feb1228512d776030a1944212b0627b
Samantha McVey <samantham@posteo.net>
2016-11-26 20:49:29 -0500

    Update README.md generator for the renamed files

--- .gen-md/gen-md.sh
+++ .gen-md/gen-md.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 snippet_dir='.gen-md'
-PERL6LIB=lib pod-render.pl6 --md ./lib/Perlbot.pm6
-mv Perlbot.md $snippet_dir/pod.md
-cd $snippet_dir
-cat head.md pod.md tail.md > ../README.md
+PERL6LIB=lib pod-render.pl6 --md ./lib/IRCPlugin/Keira.pm6 || exit
+mv Keira.md $snippet_dir/pod.md || exit
+cd $snippet_dir || exit
+cat head.md pod.md tail.md > ../README.md || exit
